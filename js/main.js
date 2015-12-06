159613c66c9d5163140a2e8cc76c1601687186a4
CarolineBenn <lineybenn@gmail.com>
2015-12-06 14:25:24 +0000

    All four ducks now move in different directions across the screen

--- js/main.js
+++ js/main.js
@@ -1,28 +1,12 @@
 $(document).ready(function(){
 
-var singleDuck = $(".duck");
+var singleDuck = $(".duck:nth-child(3)");
+var flyAwayDuck = ({ "left" : "40%", "top":"-10%"});
+   
 
-
-  // Duck - needs to work
-  TweenMax.from(".duck", 0.3, {
-  opacity:0,
-  ease: Power0.easeNone
-  });
-
-
-/*
-  $('.content')
-     .find('.duck:nth-child(2)')
-     .animate({
-        left: 280
-      }, 'slow');
-*/
-
-
-/*
 
 // NOTE: The following code works, but won't work with percentages
-
+/*
 var timeDelay1 = 1;
 var duckFlyPauseEasy = 1;
 
@@ -39,26 +23,32 @@ TweenMax.to(".duck:nth-child(1)", 1, {
   x: 0
 });
 
-
 $('.duck').click(function() {
   TweenMax.killTweensOf($(this));
 })
 */
 
 
+// Easy mode - Level One
+$(singleDuck).css({"left": "80%"});
 
-$(".duck:nth-child(1)").animate({"left":"85%", "top":"50%" }, 2000).delay(1000).animate({"left":"50%", "top":"20%"}, 1000);
+// Duck 1
+$(".duck:nth-child(1)").animate({"left":"85%", "top":"50%" }, 2000, "swing").delay(700).animate({"left":"50%", "top":"20%"}, 1000, "swing").delay(500).animate({"top":"50%", "left":"10%"}, 1250, "swing").delay(600).animate(flyAwayDuck, 400, "swing");
 
+// Duck 2
+$(".duck:nth-child(2)").animate({"left":"35%", "top":"30%" }, 1500, "swing").delay(650).animate({"left":"20%", "top":"35%"},950,"swing").delay(900).animate({"left":"70%", "top":"67%"}, 1000, "swing").delay(600).animate(flyAwayDuck, 400, "swing");
 
+// Duck 3
+$(".duck:nth-child(3)").animate({"left":"25%", "top":"60%" }, 1500, "swing").delay(500).animate({"left":"20%", "top":"35%"},800,"swing").delay(900).animate({"left":"70%", "top":"67%"}, 1000, "swing").delay(300).animate(flyAwayDuck, 400, "swing");
 
-$(".duck:nth-child(2)").animate({"left":"35%", "top":"50%" }, 2000);
+// Duck 4
+$(".duck:nth-child(4)").animate({"left":"30%", "top":"10%" }, 1700, "swing").delay(1000).animate({"left":"30%", "top":"66%"}, 1000, "swing").delay(500).animate({"top":"23%", "left":"1%"}, 1250, "swing").delay(600).animate(flyAwayDuck, 400, "swing");
+
 
 
 
 
 // On click screen flashes
-
-
 $('img.background').click(function() {
     $('img.background').addClass('flash');
     setTimeout(function() {
